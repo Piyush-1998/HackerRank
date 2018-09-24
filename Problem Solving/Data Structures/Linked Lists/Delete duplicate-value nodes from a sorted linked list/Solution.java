deebbe1e6828e0c626b4da68848bc1c932fd8b5e
Velislav Klimentov <velislav.klimentov.96@abv.bg>
2018-09-24 12:33:24 +0300

    Update Solution.java

--- Problem Solving/Data Structures/Linked Lists/Delete duplicate-value nodes from a sorted linked list/Solution.java
+++ Problem Solving/Data Structures/Linked Lists/Delete duplicate-value nodes from a sorted linked list/Solution.java
@@ -1,25 +1,25 @@
-    /*
-     * SinglyLinkedListNode {
-     *     int data;
-     *     SinglyLinkedListNode next;
-     * }
-     */
+/*
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ */
 
-    static SinglyLinkedListNode removeDuplicates(SinglyLinkedListNode head) {
+static SinglyLinkedListNode removeDuplicates(SinglyLinkedListNode head) {
 
-        SinglyLinkedListNode temp = head;
-        while(temp.next != null) {
-            
-            if(temp.data == temp.next.data) {
-                
-                temp.next = temp.next.next;
-            }
-            
-            else if (temp.next != null) {
-                
-                temp = temp.next;
-            }
+    SinglyLinkedListNode temp = head;
+    while(temp.next != null) {
+
+        if(temp.data == temp.next.data) {
+
+            temp.next = temp.next.next;
         }
-        
-        return head;
+
+        else if (temp.next != null) {
+
+            temp = temp.next;
+        }
     }
+
+    return head;
+}
