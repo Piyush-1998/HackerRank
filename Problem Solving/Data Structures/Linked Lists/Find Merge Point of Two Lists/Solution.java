bc1e85b93243a03ce5941f1e91b5a2b447e9696a
Velislav Klimentov <velislav.klimentov.96@abv.bg>
2018-09-24 12:28:38 +0300

    Update Solution.java

--- Problem Solving/Data Structures/Linked Lists/Find Merge Point of Two Lists/Solution.java
+++ Problem Solving/Data Structures/Linked Lists/Find Merge Point of Two Lists/Solution.java
@@ -1,30 +1,30 @@
-    /*
-     * SinglyLinkedListNode {
-     *     int data;
-     *     SinglyLinkedListNode next;
-     * }
-     */
+/*
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ */
 
-	static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+    
+    SinglyLinkedListNode tmp1 = head1;
+    
+    while(tmp1 != null) {
         
-        SinglyLinkedListNode tmp1 = head1;
+        SinglyLinkedListNode tmp2 = head2;
         
-        while(tmp1 != null) {
+        while(tmp2 != null) {
             
-            SinglyLinkedListNode tmp2 = head2;
-            
-            while(tmp2 != null) {
+            if(tmp1 == tmp2) {
                 
-                if(tmp1 == tmp2) {
-                    
-                    return tmp1.data;
-                }
-                
-                tmp2 = tmp2.next;
+                return tmp1.data;
             }
             
-            tmp1 = tmp1.next;
+            tmp2 = tmp2.next;
         }
         
-        return 0;
+        tmp1 = tmp1.next;
     }
+    
+    return 0;
+}
