c187d4023ac86e0200e231a1e32bab332dede853
Velislav Klimentov <velislav.klimentov.96@abv.bg>
2018-10-12 18:39:52 +0300

    Update MissingNumbers.cpp

--- Problem Solving/Algorithms/Search/Missing Numbers/MissingNumbers.cpp
+++ Problem Solving/Algorithms/Search/Missing Numbers/MissingNumbers.cpp
@@ -8,7 +8,7 @@ using namespace std;
 vector <int> missingNumbers(vector <int> arr, vector <int> brr) {
 
 	vector<int> missingNumbers;
-
+	
 	map<int, int> count_first_arr;
 	map<int, int> count_second_arr;
 
@@ -23,7 +23,6 @@ vector <int> missingNumbers(vector <int> arr, vector <int> brr) {
 
 	for (int i = 0; i < brr.size(); i++)
 		count_second_arr[brr[i]]++;
-
 
 	for (int i = 0; i < arr.size(); i++)
 	{
@@ -46,24 +45,26 @@ vector <int> missingNumbers(vector <int> arr, vector <int> brr) {
 }
 
 int main() {
+	
 	int n;
 	cin >> n;
 	vector<int> arr(n);
 	for (int arr_i = 0; arr_i < n; arr_i++) {
 		cin >> arr[arr_i];
 	}
+	
 	int m;
 	cin >> m;
 	vector<int> brr(m);
 	for (int brr_i = 0; brr_i < m; brr_i++) {
 		cin >> brr[brr_i];
 	}
+	
 	vector <int> result = missingNumbers(arr, brr);
 	for (int i = 0; i < result.size(); i++) {
 		cout << result[i] << (i != result.size() - 1 ? " " : "");
 	}
 	cout << endl;
-
 
 	return 0;
 }
