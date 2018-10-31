11c0a435adc69f9ec2804687d43ec0dd0959f803
Velislav Klimentov <velislav.klimentov.96@abv.bg>
2018-10-31 14:28:54 +0200

    Update MinimumLoss.java

--- Problem Solving/Algorithms/Search/Minimum Loss/MinimumLoss.java
+++ Problem Solving/Algorithms/Search/Minimum Loss/MinimumLoss.java
@@ -3,73 +3,73 @@ import java.util.*;
 
 public class MinimumLoss {
 
-    static class Checker implements Comparator<Pair> {
-        
-        public int compare(Pair first, Pair second) {
-            
-            return Long.compare(first.data, second.data);
-        }
-    }
-    
-    static class Pair {
-        
-        int originalIndex;
-        long data;
-        
-        Pair(int index, long data) {
-            
-            this.originalIndex = index;
-            this.data = data;
-        }
-    }
-    
-    static long minimumLoss(long[] original) {
-        
-        ArrayList<Pair> copy = new ArrayList<Pair>();
-        
-        for(int i = 0; i < original.length; i++) {
+	static class Checker implements Comparator<Pair> {
 
-            copy.add(new Pair(i, original[i]));
-        }
-        
-        Collections.sort(copy, new Checker());
+		public int compare(Pair first, Pair second) {
 
-        long minDifference = Long.MAX_VALUE;
-        for(int i = 1; i < copy.size(); i++) {
+			return Long.compare(first.data, second.data);
+		}
+	}
 
-            long currentDifference = copy.get(i).data - copy.get(i - 1).data;
-            int firstIndex = copy.get(i).originalIndex;
-            int secondIndex = copy.get(i - 1).originalIndex;
-            if((currentDifference < minDifference) && (secondIndex > firstIndex)) {
-                
-                minDifference = currentDifference;
-            }
-        }
-        
-        return minDifference;        
-    }
+	static class Pair {
 
-    private static final Scanner scanner = new Scanner(System.in);
+		int originalIndex;
+		long data;
 
-    public static void main(String[] args) throws IOException {
+		Pair(int index, long data) {
 
-        int n = scanner.nextInt();
-        scanner.nextLine();
+			this.originalIndex = index;
+			this.data = data;
+		}
+	}
 
-        long[] price = new long[n];
+	static long minimumLoss(long[] original) {
 
-        String[] priceItems = scanner.nextLine().split(" ");
+		ArrayList<Pair> copy = new ArrayList<Pair>();
 
-        for (int i = 0; i < n; i++) {
-            
-        	long priceItem = Long.parseLong(priceItems[i]);
-            price[i] = priceItem;
-        }
+		for (int i = 0; i < original.length; i++) {
 
-        long result = minimumLoss(price);
+			copy.add(new Pair(i, original[i]));
+		}
 
-        System.out.println(result);
+		Collections.sort(copy, new Checker());
 
-        scanner.close();
-    }
+		long minDifference = Long.MAX_VALUE;
+		for (int i = 1; i < copy.size(); i++) {
+
+			long currentDifference = copy.get(i).data - copy.get(i - 1).data;
+			int firstIndex = copy.get(i).originalIndex;
+			int secondIndex = copy.get(i - 1).originalIndex;
+			if ((currentDifference < minDifference) && (secondIndex > firstIndex)) {
+
+				minDifference = currentDifference;
+			}
+		}
+
+		return minDifference;
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+
+		int n = scanner.nextInt();
+		scanner.nextLine();
+
+		long[] price = new long[n];
+
+		String[] priceItems = scanner.nextLine().split(" ");
+
+		for (int i = 0; i < n; i++) {
+
+			long priceItem = Long.parseLong(priceItems[i]);
+			price[i] = priceItem;
+		}
+
+		long result = minimumLoss(price);
+
+		System.out.println(result);
+
+		scanner.close();
+	}
 }
