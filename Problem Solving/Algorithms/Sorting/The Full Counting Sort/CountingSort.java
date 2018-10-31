81c9ecb9c41570a784c056f8dd2ec56e82a9cee6
Velislav Klimentov <velislav.klimentov.96@abv.bg>
2018-10-31 14:36:22 +0200

    Update CountingSort.java

--- Problem Solving/Algorithms/Sorting/The Full Counting Sort/CountingSort.java
+++ Problem Solving/Algorithms/Sorting/The Full Counting Sort/CountingSort.java
@@ -3,40 +3,40 @@ import java.util.*;
 
 public class CountingSort {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        final int maxValue = 100;
-        
-        HashMap<Integer, List<String>> map = new HashMap<>();
-        
-        for (int i = 0; i < maxValue; i++) {
-            
-            map.put(i, new ArrayList<String>());
-        }
-        
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
-        for (int i = 0; i < n; i++) {
-            
-        	List<String> pair = Arrays.asList(bufferedReader.readLine().split(" "));
-            int key = Integer.parseInt(pair.get(0));
-            String value = (i < n / 2) ? "-" : pair.get(1);
-            
-            List<String> list = map.get(key);
-            list.add(value);
-        }
-        
-        bufferedReader.close();
+		final int maxValue = 100;
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < maxValue; i++) {
-            
-        	List<String> values = map.get(i);
-            for (String str : values) {
-                sb.append(str + " ");
-            }
-        }
-        
-        System.out.println(sb);
-    }
+		HashMap<Integer, List<String>> map = new HashMap<>();
+
+		for (int i = 0; i < maxValue; i++) {
+
+			map.put(i, new ArrayList<String>());
+		}
+
+		int n = Integer.parseInt(bufferedReader.readLine().trim());
+		for (int i = 0; i < n; i++) {
+
+			List<String> pair = Arrays.asList(bufferedReader.readLine().split(" "));
+			int key = Integer.parseInt(pair.get(0));
+			String value = (i < n / 2) ? "-" : pair.get(1);
+
+			List<String> list = map.get(key);
+			list.add(value);
+		}
+
+		bufferedReader.close();
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < maxValue; i++) {
+
+			List<String> values = map.get(i);
+			for (String str : values) {
+				sb.append(str + " ");
+			}
+		}
+
+		System.out.println(sb);
+	}
 }
